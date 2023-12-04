74601cd5085f88dece759b949ece2949506f582e
unknown <njema3@gmail.com>
2023-12-04 13:49:59 +0300

    first commit

---
+++ src/datatablesource.js
@@ -0,0 +1,123 @@
+export const userColumns = [
+  { field: "id", headerName: "ID", width: 70 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 230,
+    renderCell: (params) => {
+      return (
+        <div className="cellWithImg">
+          <img className="cellImg" src={params.row.img} alt="avatar" />
+          {params.row.username}
+        </div>
+      );
+    },
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 230,
+  },
+
+  {
+    field: "age",
+    headerName: "Age",
+    width: 100,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 160,
+    renderCell: (params) => {
+      return (
+        <div className={`cellWithStatus ${params.row.status}`}>
+          {params.row.status}
+        </div>
+      );
+    },
+  },
+];
+
+//temporary data
+export const userRows = [
+  {
+    id: 1,
+    username: "Nick",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    status: "active",
+    email: "nick@gmail.com",
+    age: 35,
+  },
+  {
+    id: 2,
+    username: "Jemima",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "jemima@gmail.com",
+    status: "passive",
+    age: 32,
+  },
+  {
+    id: 3,
+    username: "Lakewood",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "lakewood@gmail.com",
+    status: "pending",
+    age: 55,
+  },
+  {
+    id: 4,
+    username: "Dawood",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "dawood@gmail.com",
+    status: "active",
+    age: 46,
+  },
+  {
+    id: 5,
+    username: "Targaryen",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "targa@gmail.com",
+    status: "passive",
+    age: 42,
+  },
+  {
+    id: 6,
+    username: "Melisa",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "melisa@gmail.com",
+    status: "active",
+    age: 15,
+  },
+  {
+    id: 7,
+    username: "Clifford",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "clifford@gmail.com",
+    status: "passive",
+    age: 34,
+  },
+  {
+    id: 8,
+    username: "Francis",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "francis@gmail.com",
+    status: "active",
+    age: 36,
+  },
+  {
+    id: 9,
+    username: "Roxie",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "roxie@gmail.com",
+    status: "pending",
+    age: 45,
+  },
+  {
+    id: 10,
+    username: "James",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "james@gmail.com",
+    status: "active",
+    age: 55,
+  },
+];
